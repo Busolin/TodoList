a3d95f557171fc2aec2e17a93f2400367da985f2
Victor Busolin <victorbusolin@alunos.utfpr.edu.br>
2023-12-14 15:15:23 -0300

    feat: design for app.tsx and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.svg';
+import styles from './App.module.css';
+import { PlusCircle } from '@phosphor-icons/react';
+import { useState } from 'react';
+import { EmptyList } from './components/EmptyList';
+
+export interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<Task[]>([])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <img src={logo} alt="" className={styles.logo} />
+        <div className={styles.headerNewTaskWrapper}>
+          <input type="text" className={styles.headerNewTaskInput} placeholder="Adicione uma nova tarefa" />
+          <button className={styles.headerNewTaskButton}>
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </div>
+
+      </header>
+
+      <section className={styles.section}>
+        <div className={styles.sectionHeader}>
+          <p className={styles.sectionTasksCreated}>
+            <span>Tarefas criadas</span>
+            <span>0</span>
+          </p>
+
+          <p className={styles.sectionTasksCompleted}>
+            <span>Concluídas</span>
+            <span>0</span>
+          </p>
+        </div>
+
+        <div>
+          {tasks.length === 0
+            ?
+            <div className="">
+              <EmptyList />
+            </div>
+
+            :
+
+            <div className="">
+              <p>suck not empty</p>
+            </div>
+          }
+        </div>
+      </section>
+    </main>
   )
 }
 
-export default App
+export default App;
