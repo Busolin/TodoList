8e178684094fea9017f34fbd7fcd8da16fd3e2fd
Victor Busolin <victorbusolin@alunos.utfpr.edu.br>
2023-12-19 10:52:19 -0300

    feat: implemented tasks functions & localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
 import logo from './assets/logo.svg';
 import styles from './App.module.css';
 import { PlusCircle } from '@phosphor-icons/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { EmptyList } from './components/EmptyList';
-
-export interface Task {
-  id: number;
-  title: string;
-  isComplete: boolean;
-}
+import { Task, TaskProps } from './components/Task';
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([])
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState<string>('');
+  const [completedTasks, setCompletedTasks] = useState<TaskProps[]>([]);
+
+  // Getting from localStorage
+  useEffect(() => {
+    const storedTasks: TaskProps[] | null = JSON.parse(localStorage.getItem('@todoList:tasks') || '[]');
+    if (storedTasks?.length === 0) { return }
+
+    setTasks(storedTasks || []);
+  }, []);
+
+  function handleNewTaskChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTaskTitle(event.target.value);
+  }
+
+  async function generateNewTask() {
+    if (newTaskTitle === '') {
+      return;
+    }
+
+    const newTask: TaskProps = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isCompleted: false,
+      deleteTask: handleDeleteTaskClick,
+      updateTaskCompletion: handleTaskCompletion
+    }
+
+    await setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+  }
+
+  useEffect(() => {
+    updateLocalStorage();
+  }, [tasks]);
+
+  function updateLocalStorage() {
+    localStorage.setItem('@todoList:tasks', JSON.stringify(tasks));
+  }
+
+  function handleDeleteTaskClick(id: number) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  function handleTaskCompletion(id: number, isCompleted: boolean) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isCompleted };
+      }
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  }
+
+  useEffect(() => {
+    const completedTasks = tasks.filter(task => task.isCompleted);
+    setCompletedTasks(completedTasks);
+  }, [tasks]);
+
 
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <img src={logo} alt="" className={styles.logo} />
         <div className={styles.headerNewTaskWrapper}>
-          <input type="text" className={styles.headerNewTaskInput} placeholder="Adicione uma nova tarefa" />
-          <button className={styles.headerNewTaskButton}>
+          <input type="text" className={styles.headerNewTaskInput} placeholder="Adicione uma nova tarefa" value={newTaskTitle}
+            onChange={handleNewTaskChange} />
+          <button className={styles.headerNewTaskButton} onClick={generateNewTask}>
             Criar
             <PlusCircle size={16} />
           </button>
@@ -31,26 +87,40 @@ function App() {
         <div className={styles.sectionHeader}>
           <p className={styles.sectionTasksCreated}>
             <span>Tarefas criadas</span>
-            <span>0</span>
+            <span>{tasks.length}</span>
           </p>
 
           <p className={styles.sectionTasksCompleted}>
             <span>Concluídas</span>
-            <span>0</span>
+            <span>
+              {completedTasks.length !== 0
+                ?
+                `${completedTasks.length} de ${tasks.length}`
+                :
+                0
+              }
+            </span>
           </p>
         </div>
 
         <div>
           {tasks.length === 0
             ?
-            <div className="">
-              <EmptyList />
-            </div>
-
+            <EmptyList />
             :
 
-            <div className="">
-              <p>suck not empty</p>
+            <div className={styles.tasksWrapper}>
+              {
+                tasks.map(task => (
+                  <Task key={task.id}
+                    id={task.id}
+                    title={task.title}
+                    isCompleted={task.isCompleted}
+                    deleteTask={handleDeleteTaskClick}
+                    updateTaskCompletion={handleTaskCompletion}
+                  />
+                ))
+              }
             </div>
           }
         </div>
